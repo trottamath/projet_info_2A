c4c1c4216daaa1004d63f0f79bd41f099348c1f0
Jean-Phi <trotta.math@gmail.com>
2022-09-25 12:12:43 +0200

    add

---
+++ Application/DAO/creation_tables.py
@@ -0,0 +1,18 @@
+"""création des tables pour accueillir les bases de données sur PostgreSQL (à compléter)"""
+
+"""
+CREATE TABLE parcelle(
+    id_par TEXT PRIMARY KEY,
+    id_parc TEXT,
+    id_com_inclus TEXT,
+    limite_com BOOLEAN,
+    nom_commune TEXT,
+    date TEXT,
+);
+
+
+
+
+"""
+
+
