6ad3ab6ae8d336b7042f13d4a5f1bdcb39a54912
Jean-Phi <trotta.math@gmail.com>
2022-09-30 11:53:16 +0200

    import

---
+++ Application/metier/import_export/web.py
@@ -0,0 +1,41 @@
+class Web():
+
+    def __init__(self, id_dep : str, date = "latest" , zonage1 = "departements", id_zone = None, zonage2 = "communes" ):
+        """lien de l'adresse d'import d'un fichier json à partir du web
+        Attirbut :
+        ----------
+            url : str
+        Parametres:
+        -----------
+            date : str = "latest" 
+                sinon saisir au format "AAAA-MM-JJ"
+            zonage1 : str = "departements"
+                zonage au choix parmi "departements" ou "france" ou "communes"
+            id_dep : str
+                code de département
+            id_zone : str = None
+                saisir le code communes si zone1="communes"
+            zonage2 : str = "communes"
+                zonage au choix parmi "parcelles" ou "communes"
+                sauf si zonage1="france" alors laisser par défaut zonage2="communes"
+        
+        """
+
+        url = "https://cadastre.data.gouv.fr/data/etalab-cadastre/"
+
+        if zonage1 == "departements":
+            id_zone = id_dep
+            url_zone1 = id_dep
+        elif zonage1 == "communes":
+            url_zone1="{}/{}".format(id_dep,id_zone)
+
+        fichier ="cadastre-{}-{}.json.gz".format(id_zone,zonage2)
+
+        if zonage1 == "france" and zonage2== "communes":
+            self.url = "https://cadastre.data.gouv.fr/data/etalab-cadastre/latest/geojson/france/cadastre-france-communes.json.gz"
+        else:
+            self.url = "{}{}/geojson/{}/{}/{}".format(url,date,zonage1,url_zone1,fichier)
+
+
+
+
