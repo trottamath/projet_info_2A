c1775d6f5f46b7b4df6c63f31fbb8fbcbf644b36
TROTTA Jean-philippe <id1964@DOMENSAI.ECOLE>
2022-10-14 15:46:25 +0200

    bdr

--- Application/init_db.sql
+++ Application/init_db.sql
@@ -1,17 +1,27 @@
+DROP TABLE IF EXISTS commune CASCADE;
+CREATE TABLE commune (
+	id_com TEXT PRIMARY KEY,
+    nom_commune TEXT
+);
+
 DROP TABLE IF EXISTS parcelle CASCADE;
 CREATE TABLE parcelle (
 	id_parc TEXT PRIMARY KEY,
-	id_com_inclus TEXT,
-    limite_com BOOLEAN,
-    nom_commune TEXT,
-    date TEXT
+	id_com_limit TEXT REFERENCES commune(id_com)
 );
 
 
-DROP TABLE IF EXISTS parcelle_parcelle CASCADE;
-CREATE TABLE parcelle_parcelle (
-    id_parc1 TEXT,
-    id_parc2 TEXT,
+DROP TABLE IF EXISTS parcelle_commune CASCADE;
+CREATE TABLE parcelle_commune (
+    id_parc TEXT REFERENCES parcelle(id_parc),
+    id_com TEXT REFERENCES commune(id_com),
+    date TEXT
+);
+
+DROP TABLE IF EXISTS commune_commune CASCADE;
+CREATE TABLE commune_commune (
+    id_com1 TEXT REFERENCES commune(id_com),
+    id_com2 TEXT REFERENCES commune(id_com),
     date TEXT
 );
 
