c0ca0709d3a4651fd22ed424a983609dff5c7c6f
eva-puch <eva.puch@yahoo.fr>
2022-10-26 16:23:19 +0200

    methode requeter service_dao

--- Application/service/service_dao.py
+++ Application/service/service_dao.py
@@ -1,19 +1,25 @@
 from Application.dao.commune_commune_dao import CommuneCommuneDAO
 from Application.dao.parcelle_dao import ParcelleDAO
 
-class Voisines ():
+class Requete ():
     """Classe qui lie le Service à la DAO : envoie de la requête
     à la DAO, et récupération du résultat"""
 
-    def requete(self, id:str, date:str):
-        if requete == 1 : # voisines communes à une voisine donnée
+    def __init__(self, num:str) :
+        """num : int, numero de la requete (1 ou 2 dans le cas de l'appel à la DAO"""
+        self.requete = num
+            
+
+    def requeter(self, id:str, date:str):
+        """id : str, identifiant de la commune ou de la parcelle à traiter"""
+        if self.requete == 1 : # voisines communes à une voisine donnée
             com = CommuneCommuneDAO()
             res = com.recherche_com(id_com, date)
         
-        elif requete == 2 : # parcelles en bordure d'une commune donnée
+        elif self.requete == 2 : # parcelles en bordure d'une commune donnée
             com = ParcelleDAO()
             res = com.research_all_lim(id)
 
-        #else : # requete == 3 : parcelles voisines d'une parcelle donnée
-            #parc = ParcelleDAO()
-            #res = parc.
+        return res
+
+
