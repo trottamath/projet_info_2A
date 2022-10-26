33cd31e598081d49a296bde93c580afca38b0576
Chlcnt <chloe.cnt@laposte.net>
2022-10-26 15:01:20 +0200

    modif telechargement

--- Application/client/test.py
+++ Application/client/test.py
@@ -1,15 +1,15 @@
 import requests
-import gzip
+#import gzip
 
 downloadUrl ='https://cadastre.data.gouv.fr/data/etalab-cadastre/latest/geojson/communes/04/04004/cadastre-04004-communes.json.gz'
 
 req = requests.get(downloadUrl)
 
-#print(req.headers)
+print(req.headers)
 
 filename = req.url[downloadUrl.rfind('/')+1:]
 
-#print(filename)
+print(filename)
 
 
 #téléchargement du json.gz
@@ -18,7 +18,9 @@ with open(filename, 'wb') as f:
     for data in req.iter_content():
             f.write(data)
 
-#téléchargement du json et trasnfère dans le fichier data(inputs)
+#téléchargement du json et transfère dans le fichier data(inputs)
+
+
 
 #lecture du json.gz
 with gzip.GzipFile(filename, 'rb') as f:
