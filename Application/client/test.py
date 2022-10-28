5cb1a2054094f28f411d1e3f0b86206be1e8fc68
TROTTA Jean-philippe <id1964@DOMENSAI.ECOLE>
2022-10-28 11:13:27 +0200

    test

--- Application/client/test.py
+++ Application/client/test.py
@@ -5,25 +5,25 @@ import os
 import json
 
 
-downloadUrl ='https://cadastre.data.gouv.fr/data/etalab-cadastre/latest/geojson/communes/04/04004/cadastre-04004-communes.json.gz'
+# downloadUrl = r'https://cadastre.data.gouv.fr/data/etalab-cadastre/latest/geojson/communes/04/04004/cadastre-04004-communes.json.gz'
 
-req = requests.get(downloadUrl)
+# req = requests.get(downloadUrl)
 
-print(req.headers)
+# print(req.headers)
 
-filename = req.url[downloadUrl.rfind('/')+1:]
-path = 'Application/Client/data/communes' #coder le chemin en fonction du zonage_1 (commune ou département (cf. nom du fichier))
+# filename = req.url[downloadUrl.rfind('/')+1:]
+# path = 'Application/Client/data/communes' #coder le chemin en fonction du zonage_1 (commune ou département (cf. nom du fichier))
 
-print(filename)
+# print(filename)
 
-print(os.path.join(path,filename))
+# print(os.path.join(path,filename))
 
 
-#téléchargement du json.gz dans le fichier "data"
+# #téléchargement du json.gz dans le fichier "data"
 
-with req as rq:
-    with gzip.open(os.path.join(path,filename), 'wb') as file: #possibilité de changer le nom du fichier, ex : 'data.json.gz' au lieu de filename
-        file.write(rq.content)
+# with req as rq:
+#     with open("test.json.gz", 'wb') as file: #possibilité de changer le nom du fichier, ex : 'data.json.gz' au lieu de filename
+#         file.write(rq.content)
 
 #lecture du json.gz sous forme de dictionnaire depuis un fichier donné
 
@@ -31,8 +31,8 @@ with req as rq:
 # data_files_names = data_files_names =[data_folder]
 # data_folder[0]
 
-with gzip.open(filename,'rb') as f_in :
-    with open('json_decomp.json','wb') as f_out:
+with gzip.open("test.json.gz",'rb') as f_in :
+    with open('client\data\communes\json_decomp.json','wb') as f_out:
         shutil.copyfileobj(f_in,f_out)
 
 #with gzip.open(filename,'rb') as file:
