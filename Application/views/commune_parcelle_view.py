194d032e148fd49e38c3f8d0ca642aa42a51fa7e
TROTTA Jean-philippe <id1964@DOMENSAI.ECOLE>
2022-10-28 12:29:34 +0200

    view

--- Application/views/commune_parcelle_view.py
+++ Application/views/commune_parcelle_view.py
@@ -5,23 +5,20 @@ from prompt_toolkit.validation import ValidationError, Validator
 from PyInquirer import prompt
 
 from views.abstract_view import AbstractView
-from view.session import Session
+from views.session import Session
+from service.requete import Requete
 
 class Commune_Parcelle_View(AbstractView):
     def __init__(self) -> None:
         self.__questions = [
             {
                 'type': 'input',
-                'name': 'identifiant de la commune',
-                'message':'quel est l\'identifiant du zonage'
-            },
-            {
-                'type':'input',
-                'name':'date',
-                'message':'quel est la date correspondant à votre recherche'
-
+                'name': 'id_commune',
+                'message':'quel est l\'identifiant de la zone de référence'
             }
         ]
+
+
 
     def display_info(self):
         print(f"Hello {Session().user_name}, veuillez choisr un bon identifiant de commune")
@@ -29,4 +26,9 @@ class Commune_Parcelle_View(AbstractView):
     def make_choice(self):
         answers = prompt(self.__questions)
         pprint(answers)
+        Session().id=answers['id_commune']
+        dico_requete= {"num":Session().num,"id":Session().id,"date":Session().date}
+        Session().list_res = Requete(dico_requete= dico_requete ).Get_or_create()
+        pprint(Session().list_res) 
+
         from views.start_view import StartView
--- Application/views/start_view.py
+++ Application/views/start_view.py
@@ -15,6 +15,26 @@ class StartView(AbstractView):
                 ,Choice('Liste des parcelles limitrophes à une parcelle donnée')
                 ,Choice('Aucun')
             ]
+        self.__questions2 = inquirer.select(
+            message=f'Bonjour{Session().user_name}'
+            , choices=[
+                Choice('2019-01-01')
+                ,Choice('2019-04-01')
+                ,Choice('2019-07-01')
+                ,Choice('2019-10-01')
+                ,Choice('2020-01-01')
+                ,Choice('2020-07-01')
+                ,Choice('2020-10-01')
+                ,Choice('2021-02-01')
+                ,Choice('2021-04-01')
+                ,Choice('2021-07-01')
+                ,Choice('2021-10-01')
+                ,Choice('2022-01-01')
+                ,Choice('2022-04-01')
+                ,Choice('2022-07-01')
+
+                ,Choice('latest')
+            ]
         )
     def display_info(self):
         with open('graphical_assets/banner.txt','r',encoding="utf_8") as asset:
@@ -22,18 +42,19 @@ class StartView(AbstractView):
     
     def make_choice(self):
         reponse = self.__questions.execute()
+        Session().date=self.__questions1.execute()
         if reponse == 'Aucun':
             pass
         elif reponse == 'Liste des communes limitrophes à une commune donnée':
-            num="1"
+            Session().num="1"
             from views.commune_commune_view import Commune_Commune_View
             return Commune_Commune_View()
         elif reponse == 'Liste des parcelles en bordure d\'une commune donnée':
-            num="2"
+            Session().num="2"
             from views.commune_parcelle_view import Commune_Parcelle_View
             return Commune_Parcelle_View()
         elif reponse == 'Liste des parcelles limitrophe à une parcelle donnée':
-            num="3"
+            Session().num="3"
             from views.parcelle_parcelle_view import Parcelle_Parcelle_View
             return Parcelle_Parcelle_View()
 
