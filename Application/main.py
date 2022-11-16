0b5990d060562224d0abd02ef7f06c123af5cbb6
id2178 <fionakarlie.f@yahoo.com>
2022-11-16 12:00:33 +0100

    main modified

--- Application/main.py
+++ Application/main.py
@@ -1,2 +1,17 @@
 #copier le code du tp pour lancer le programme dans la console
+from views.start_view import StartView
 
+if __name__ == '__main__':
+    #run the start view
+    current_view = StartView()
+
+    while current_view:
+        with open('graphical_assets/border.txt','r', encoding="utf-8") as asset:
+            print(asset.read())
+
+        current_view.display_info()
+
+        current_view = current_view.make_choice()
+
+    with open('graphical_assets/suprised_pikachu.txt', 'r', encoding="utf-8") as asset:
+        print(asset.read())
