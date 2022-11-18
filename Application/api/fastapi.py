33360238ab369ed63bfa9ac36fe6e73cb1d54ec0
Justine Farnarier <justine.farnarier@eleve.ensai.fr>
2022-11-18 12:42:54 +0100

    api

--- Application/api/fastapi.py
+++ Application/api/fastapi.py
@@ -5,4 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-    
+
+# Lancement de l'application 
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=80) 
