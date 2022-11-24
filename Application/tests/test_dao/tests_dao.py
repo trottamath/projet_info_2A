c104b32cc90030d7dfcc92f301a80000d7651db3
PUCHALSKI Eva <id1932@DOMENSAI.ECOLE>
2022-11-24 17:26:17 +0100

    tests

--- Application/tests/test_dao/tests_dao.py
+++ Application/tests/test_dao/tests_dao.py
@@ -1,10 +1,18 @@
 ############################### Tests commune_dao ###################################
 
 from dao.commune_dao import CommuneDAO
+from dao.parcelle_dao import ParcelleDAO
+from dao.commune_commune_dao import CommuneCommuneDAO
+from dao.parcelle_commune_dao import ParcelleCommuneDAO
 import unittest
 
 class CommuneDAOTest(unittest.TestCase):
     
     def test_nom_commune(self):
         c = CommuneDAO()
-        res1 = c.nom_com # ??
+        #GIVEN
+        res1 = '' ###
+        #WHEN
+        res2 = c.nom_commune('') ###
+        #THEN
+        self.assertAlmostEquals(res1, res2)
