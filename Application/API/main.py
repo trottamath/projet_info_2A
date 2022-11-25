70f0a90e36fc9b3095e3f5e528deb1bdcccc9a79
FONKOU DAHVE Fiona karlie <id2178@DOMENSAI.ECOLE>
2022-11-25 19:02:34 +0100

    Api

--- Application/API/main.py
+++ Application/API/main.py
@@ -8,3 +8,6 @@ if __name__ == "__main__":
 #exemple de test pour la requete1 en latest: commune 13207 reponses attendues 13201, 13206 et 13208 
 
 # exemple de vrai numero de parcelle pour les tests (de la requette 3) 13001000AB0237
+
+#pour lancer l'API sur le web on entre le lien suivant:
+#http://127.0.1.0/docs#/
