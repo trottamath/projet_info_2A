0256c68109bb66efe4cff905797cda79e271cb20
TROTTA Jean-philippe <id1964@DOMENSAI.ECOLE>
2022-11-26 05:48:51 +0100

    initia

--- Application/service/initialisation_bdd.py
+++ Application/service/initialisation_bdd.py
@@ -15,7 +15,7 @@ class InitialisationBdD():
         '''méthode qui charge la base de données'''
         list_com = Instanciation(zonage1= "departements", id1= self.id_dep, zonage2= "communes", date= self.date).instancier_zonage()
         for commune in list_com:
-            Requete(dico_requete= {"num": "1", "id": self.id_dep, "date": self.date}).Get_client()
-            Requete(dico_requete= {"num": "2", "id": commune.id, "date": self.date}).Get_client()
+            Requete(dico_requete= {"num": "1", "id": commune.id, "date": self.date}).Get_Client()
+            Requete(dico_requete= {"num": "2", "id": commune.id, "date": self.date}).Get_Client()
 
-#InitialisationBdD(id_dep="13").chargement_bdd()
+InitialisationBdD(id_dep="13").chargement_bdd()
